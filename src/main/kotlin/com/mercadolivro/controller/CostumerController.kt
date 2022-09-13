4c99b4b401640a146ef53b9445dab0b2ae7573b3
Guilherme Espinola <gwanderleyespinola@gmail.com>
2022-09-13 14:44:21 -0300

    Implementando a camada service e mudnado endpoints

--- src/main/kotlin/com/mercadolivro/controller/CostumerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CostumerController.kt
@@ -3,6 +3,7 @@ package com.mercadolivro.controller
 import com.mercadolivro.controller.request.PostCostumerRequest
 import com.mercadolivro.controller.request.PutCostumerRequest
 import com.mercadolivro.model.CostumerModel
+import com.mercadolivro.service.CostumerService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -10,45 +11,36 @@ import kotlin.math.cos
 
 @RestController
 @RequestMapping("costumer")
-class CostumerController {
-
-    val costumers = mutableListOf<CostumerModel>()
+class CostumerController(
+    val costumerService : CostumerService //forma de fazer injeção de dependência no Kotlin
+) {
 
     @GetMapping
-    fun getAll(): List<CostumerModel> {
-        return costumers
+    fun getAll(@RequestParam name: String?): List<CostumerModel> {
+        return costumerService.getAll(name)
     }
 
     @GetMapping("/{id}")
     fun getById(@PathVariable id: String): CostumerModel {
-        return costumers.filter { it.id == id }.first()
+        return costumerService.getById(id)
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody costumer: PostCostumerRequest) {
-        val id = if (costumers.isEmpty()){
-            1
-        }else {
-            costumers.last().id.toInt() + 1
-        }.toString()
-
-        costumers.add(CostumerModel(id, costumer.name, costumer.email))
+        costumerService.create(costumer)
     }
 
     @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT) //response status padrão para PUT e PATCH
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun update(@PathVariable id: String, @RequestBody costumer: PutCostumerRequest) {
-        costumers.filter { it.id == id }.first().let {
-            it.name = costumer.name
-            it.email = costumer.email
-        }
+        costumerService.update(id, costumer)
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT) //response status padrão para PUT e PATCH
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun delete(@PathVariable id: String) {
-        costumers.removeIf { it.id == id }
+        costumerService.delete(id)
     }
 
 }
