e6e9b2b49c675710f8afdc2e5531a665d9e2be65
Guilherme Espinola <gwanderleyespinola@gmail.com>
2022-09-14 18:10:54 -0300

    Adicionando Status no Customer e ajustando deleções

--- src/main/kotlin/com/mercadolivro/controller/CostumerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CostumerController.kt
@@ -33,7 +33,8 @@ class CostumerController(
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun update(@PathVariable id: Int, @RequestBody customer: PutCustomerRequest) {
-        customerService.update(customer.toCustomerModel(id))
+        val customerSaved = customerService.findById(id)
+        customerService.update(customer.toCustomerModel(customerSaved))
     }
 
     @DeleteMapping("/{id}")
