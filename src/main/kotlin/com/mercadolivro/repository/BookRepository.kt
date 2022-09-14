6a3a24624550fd3fc00b27d97ed515846bc493ef
Guilherme Espinola <gwanderleyespinola@gmail.com>
2022-09-14 15:06:45 -0300

    PUT, DELETE e ExtensionFunctions para book

--- src/main/kotlin/com/mercadolivro/repository/BookRepository.kt
+++ src/main/kotlin/com/mercadolivro/repository/BookRepository.kt
@@ -1,8 +1,12 @@
 package com.mercadolivro.repository
 
+import com.mercadolivro.enums.BookStatus
 import com.mercadolivro.model.BookModel
 import org.springframework.data.repository.CrudRepository
 
 interface BookRepository : CrudRepository<BookModel, Int> {
+
     fun findByNameContaining(bookName: String)
+
+    abstract fun findByStatus(status: BookStatus): List<BookModel>
 }
