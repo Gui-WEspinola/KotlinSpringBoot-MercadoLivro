958339615151d0dcf8983caeb2ab2e2cf8e01259
Guilherme Espinola <gwanderleyespinola@gmail.com>
2022-09-14 18:49:15 -0300

    Alterando o retorno dos Controllers para BookResponse.kt e CustomerResponse.kt

--- src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -1,9 +1,10 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.controller.extension.toBookModel
+import com.mercadolivro.controller.extension.toResponse
 import com.mercadolivro.controller.request.PostBookRequest
 import com.mercadolivro.controller.request.PutBookRequest
-import com.mercadolivro.model.BookModel
+import com.mercadolivro.controller.response.BookResponse
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
@@ -32,17 +33,17 @@ class BookController(
     }
 
     @GetMapping
-    fun findAll(): List<BookModel>{
-        return bookService.findAll()
+    fun findAll(): List<BookResponse>{
+        return bookService.findAll().map { it.toResponse() }
     }
 
     @GetMapping("/active")
-    fun findActives(): List<BookModel> =
-         bookService.findActives()
+    fun findActives(): List<BookResponse> =
+         bookService.findActives().map { it.toResponse() }
 
     @GetMapping("/{id}")
-    fun findById(@PathVariable id: Int): BookModel{
-        return bookService.findById(id)
+    fun findById(@PathVariable id: Int): BookResponse{
+        return bookService.findById(id).toResponse()
     }
 
     @PutMapping("{id}")
--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,27 +1,28 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.controller.extension.toCustomerModel
+import com.mercadolivro.controller.extension.toResponse
 import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.controller.request.PutCustomerRequest
-import com.mercadolivro.model.CustomerModel
+import com.mercadolivro.controller.response.CustomerResponse
 import com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("costumer")
-class CostumerController(
+class CustomerController(
     val customerService : CustomerService //forma de fazer injeção de dependência no Kotlin
 ) {
 
     @GetMapping
-    fun getAll(@RequestParam name: String?): List<CustomerModel> {
-        return customerService.getAll(name)
+    fun getAll(@RequestParam name: String?): List<CustomerResponse> {
+        return customerService.getAll(name).map { it.toResponse() }
     }
 
     @GetMapping("/{id}")
-    fun getById(@PathVariable id: Int): CustomerModel {
-        return customerService.findById(id)
+    fun getById(@PathVariable id: Int): CustomerResponse {
+        return customerService.findById(id).toResponse()
     }
 
     @PostMapping
