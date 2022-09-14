f4b7f0bffba4509c32078a0b43ea58fb5d169c98
Guilherme Espinola <gwanderleyespinola@gmail.com>
2022-09-14 10:24:06 -0300

    Criando migration e Model de books

---
+++ src/main/kotlin/com/mercadolivro/model/BookModel.kt
@@ -0,0 +1,27 @@
+package com.mercadolivro.model
+
+import com.mercadolivro.enums.BookStatus
+import java.math.BigDecimal
+import javax.persistence.*
+
+@Entity(name = "book")
+data class BookModel (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null,
+
+    @Column
+    var name: String,
+
+    @Column
+    var price: BigDecimal,
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: BookStatus? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id") //aponta para a chave primária do customer, assim como no SQL
+    var customer: CustomerModel? = null
+)
---
+++ src/main/resources/db/migration/V2__create_table_book.sql
@@ -0,0 +1,8 @@
+create table book(
+    id int primary key auto_increment,
+    name varchar(255) not null,
+    price decimal(10,2) not null,
+    status varchar(255) not null,
+    customer_id int not null,
+    foreign key (customer_id) references customer(id)
+);
