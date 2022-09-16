f5b537058b5a17e0589cd1c86aaf64f8c7c65729
Guilherme Espinola <gwanderleyespinola@gmail.com>
2022-09-15 21:04:42 -0300

    Criando BadRequestException.kt

--- src/main/kotlin/com/mercadolivro/enums/Errors.kt
+++ src/main/kotlin/com/mercadolivro/enums/Errors.kt
@@ -3,5 +3,7 @@ package com.mercadolivro.enums
 enum class Errors(val code: String, val message: String) {
 
     ML101("ML-0001", "Book [%s] does not exist."),
+    ML102("ML-102", "Cannot update book with status [%s]"),
+
     ML201("ML-0002", "Customer [%s] does not exist.")
 }
