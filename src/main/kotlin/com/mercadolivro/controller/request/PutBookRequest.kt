54cf00a34a648fd8ef7c2b818f3c0c7356a88b4f
Guilherme Espinola <gwanderleyespinola@gmail.com>
2022-09-15 22:13:59 -0300

    Adicionando validações em outros endpoints

--- src/main/kotlin/com/mercadolivro/controller/request/PutBookRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/PutBookRequest.kt
@@ -1,10 +1,15 @@
 package com.mercadolivro.controller.request
 
 import java.math.BigDecimal
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 data class PutBookRequest (
 
+    @field: NotEmpty(message = "Name must be valid.")
     var name: String?,
+
+    @field: NotNull(message = "Price cannot be null.")
     var price: BigDecimal?
 
 )
--- src/main/kotlin/com/mercadolivro/controller/request/PutCustomerRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/PutCustomerRequest.kt
@@ -1,7 +1,13 @@
 package com.mercadolivro.controller.request
 
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
+
 data class PutCustomerRequest ( //na prática, é um DTO
 
+    @field: NotEmpty(message = "Nome deve ser informado.")
     var name: String,
+
+    @field: Email(message = "E-mail deve ser válido.")
     var email: String
 )
