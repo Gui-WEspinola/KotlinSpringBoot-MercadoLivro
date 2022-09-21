0e1b36cb73fda1f63403de505058d5cb6c5e301d
Guilherme Espinola <gwanderleyespinola@gmail.com>
2022-09-21 12:52:36 -0300

    Criando primeiros testes unitários com Junit e MockK - Adicionado Jacoco plugin no gradle

---
+++ src/test/kotlin/com/mercadolivro/service/CustomerServiceTest.kt
@@ -0,0 +1,71 @@
+package com.mercadolivro.service
+
+import com.mercadolivro.enums.CustomerStatus
+import com.mercadolivro.model.CustomerModel
+import com.mercadolivro.repository.CustomerRepository
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.util.*
+
+@ExtendWith(MockKExtension::class)
+class CustomerServiceTest{
+
+    @MockK
+    private lateinit var customerRepository: CustomerRepository
+
+    @MockK
+    private lateinit var bookService: BookService
+
+    @InjectMockKs
+    private lateinit var customerService: CustomerService
+
+    @Test
+    fun `should return all customers`(){
+
+        val customer1 = buildCustomer()
+        val fakeCustomers = listOf(buildCustomer(), buildCustomer())
+
+        every { customerRepository.findAll() } returns fakeCustomers
+
+        val customers = customerService.getAll(null)
+
+        assertEquals(fakeCustomers, customers)
+        verify(exactly = 1) { customerRepository.findAll() }
+        verify(exactly = 0){ customerRepository.findByNameContaining(any()) }
+    }
+
+    @Test
+    fun `should return customers when name is informed`(){
+        val name = Math.random().toString()
+        val fakeCustomers = listOf(buildCustomer(), buildCustomer())
+
+        every { customerRepository.findByNameContaining(name) } returns fakeCustomers
+
+        val customers = customerService.getAll(name)
+
+        assertEquals(fakeCustomers, customers)
+        verify(exactly = 0) { customerRepository.findAll() }
+        verify(exactly = 1){ customerRepository.findByNameContaining(name) }
+    }
+
+    fun buildCustomer(
+        id: Int? = null,
+        name: String = "customer name",
+        email: String = "${UUID.randomUUID()}@email.com",
+        password: String = "password"
+    ) = CustomerModel(
+        id = id,
+        name = name,
+        email = email,
+        password = password,
+        status = CustomerStatus.ATIVO
+    )
+
+
+}
